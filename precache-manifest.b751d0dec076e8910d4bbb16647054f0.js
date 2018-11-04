5afb9d5f46691c0fe3b38ba23fb7c668c8273649
John Hawthorn <john@hawthorn.email>
2018-11-03 21:19:39 -0700

    Updates

---
+++ precache-manifest.b751d0dec076e8910d4bbb16647054f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9932a764fea2ff5be5265fd8c5c53c",
+    "url": "/fzy-demo/static/media/rails_files.2e9932a7.txt"
+  },
+  {
+    "revision": "89836e3aeb9e95325050",
+    "url": "/fzy-demo/static/js/runtime~main.89836e3a.js"
+  },
+  {
+    "revision": "e37e680001d2ffcad2bb",
+    "url": "/fzy-demo/static/js/main.e37e6800.chunk.js"
+  },
+  {
+    "revision": "6f6c9852737242854fe3",
+    "url": "/fzy-demo/static/js/1.6f6c9852.chunk.js"
+  },
+  {
+    "revision": "e37e680001d2ffcad2bb",
+    "url": "/fzy-demo/static/css/main.43b688c7.chunk.css"
+  },
+  {
+    "revision": "45ce8554adf6ba2498d43d5df851ce1d",
+    "url": "/fzy-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04842edd9c3adb35bd78552ebd8744b6.js"
+  "/fzy-demo/precache-manifest.b751d0dec076e8910d4bbb16647054f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fzy-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
