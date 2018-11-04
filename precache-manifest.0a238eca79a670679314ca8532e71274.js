2d0e534e4baf28ea9d169e127ec3fe17d3ba734d
John Hawthorn <john@hawthorn.email>
2018-11-04 13:34:29 -0800

    Updates

---
+++ precache-manifest.0a238eca79a670679314ca8532e71274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9932a764fea2ff5be5265fd8c5c53c",
+    "url": "/fzy-demo/static/media/rails_files.2e9932a7.txt"
+  },
+  {
+    "revision": "b71c3ae457c8b4232beaf6e829ae7c3b",
+    "url": "/fzy-demo/static/media/fzy.b71c3ae4.svg"
+  },
+  {
+    "revision": "89836e3aeb9e95325050",
+    "url": "/fzy-demo/static/js/runtime~main.89836e3a.js"
+  },
+  {
+    "revision": "e8cfa008907f9a43ad31",
+    "url": "/fzy-demo/static/js/main.e8cfa008.chunk.js"
+  },
+  {
+    "revision": "b7cee7d5671d284f931f",
+    "url": "/fzy-demo/static/js/1.b7cee7d5.chunk.js"
+  },
+  {
+    "revision": "e8cfa008907f9a43ad31",
+    "url": "/fzy-demo/static/css/main.effa1c8a.chunk.css"
+  },
+  {
+    "revision": "098c1ed7ba25cd110b9e2a2628175469",
+    "url": "/fzy-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fzy-demo/precache-manifest.bce3624fa2fc992786996d826d47d89e.js"
+  "/fzy-demo/precache-manifest.0a238eca79a670679314ca8532e71274.js"
 );
 
 workbox.clientsClaim();
