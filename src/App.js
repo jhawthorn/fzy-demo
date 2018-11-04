7f8dc8fadc5db6d9b8581465d4a7ef993f1b3137
John Hawthorn <john@hawthorn.email>
2018-11-03 21:03:34 -0700

    Working filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import rails_files from './rails_files.txt';
+
+function filterResults(data, query) {
+  var regex = new RegExp(query.split('').join('.*'));
+
+  var results = data;
+
+  results = results.filter((x) => x.match(regex));
+
+  results = results.slice(0,10);
+
+  return results.map((x) => <li>{x}</li>);
+}
+
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null};
+
+    fetch(rails_files)
+      .then((response) => response.text())
+      .then((txt) => txt.split("\n"))
+      .then((data) => this.setState({ data: data }));
+  }
+
   render() {
+    if (!this.state.data) {
+      return <div>Loading...</div>
+    }
     return (
-      <ul class="App-results">
-        <li>{this.props.query}</li>
+      <ul className="App-results">
+        {
+          filterResults(this.state.data, this.props.query)
+        }
       </ul>
     )
   }
@@ -28,9 +57,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src="http://i.hawth.ca/u/fzy.svg" className="App-logo" alt="logo" />
-          <div class="App-ui">
-            <div class="App-prompt">
-              <span>&gt;&nbsp;</span><input type="text" class="App-input" value={this.state.query} onChange={this.handleChange} />
+          <div className="App-ui">
+            <div className="App-prompt">
+              <span>&gt;&nbsp;</span><input type="text" className="App-input" value={this.state.query} onChange={this.handleChange} />
             </div>
             <Results query={this.state.query} />
           </div>
